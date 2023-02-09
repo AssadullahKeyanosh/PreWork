5c526ecf2021be9d4f15c9d746d9cc8489cfe12a
Code Sandbox <hello@codesandbox.io>
2023-02-09 11:07:45 +0000

    Deployment from Codesandbox.

--- sketch.js
+++ sketch.js
@@ -17,14 +17,26 @@ function draw() {
 
   strokeWeight(1);
   fill(255, 255, 255); // snowman color
+
   ellipse(200, 120, 75, 75); //top ellipse
   ellipse(200, 200, 100, 100); // central ellips
   ellipse(200, 300, 150, 150); // bottom ellipse
 
+  fill(0, 0, 0); // eye color
+  ellipse(190, 105, 10, 10); //left eye
+  ellipse(210, 105, 10, 10); //left eye
+
+  fill(200, 22, 22);
+  ellipse(200, 135, 30, 20); //mouth
+
   fill(255, 205, 180); // ground color
   rect(0, 360, 398, 35); // ground
 
-  strokeWeight(4);
+  strokeWeight(10);
+  stroke(255, 205, 180);
+  line(200, 115, 190, 120); // nose
+
+  strokeWeight(10);
   stroke(139, 140, 145);
   line(50, 120, 153, 185); // right hand
   line(350, 120, 247, 185); // left hand
